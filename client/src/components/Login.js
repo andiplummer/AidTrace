cc2df6ff5acf8fa11d830d03d01a90fa01da4064
Emma Curtis <ejcurtis@dons.usfca.edu>
2019-12-07 15:49:50 -0500

    created tourus login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import Nav from './Nav'
+import Torus from "@toruslabs/torus-embed";
+import Web3 from "web3";
 
-const Login = () => {
+
+const Login = async () => {
+  const torus = new Torus({
+    buttonPosition: "top-left" // default: bottom-left
+  });
+  await torus.init({
+    buildEnv: "production", // default: production
+    enableLogging: true, // default: false
+    network: {
+      host: "localhost", // default: mainnet
+      chainId: 1,
+      networkName: "localhost 8545" // default: Main Ethereum Network
+    },
+    showTorusButton: true // default: true
+  });
+  await torus.ethereum.enable(); // await torus.ethereum.enable()
+  const web3 = new Web3(torus.provider);
   return (
     <div>
       <Nav />
