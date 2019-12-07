d1e3fc323f5343f8bcde25a315a03837d997cc0c
Lee Faria <>
2019-12-07 17:58:00 -0500

    added some files to testing

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,14 +1,14 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const AidTrace= artifacts.require("./AidTrace.sol");
 
-contract("SimpleStorage", accounts => {
+contract("AidTrace, accounts => {
   it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+    const aidTracenstance = await Aidtrace.deployed();
 
     // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    await aidTraceInstance.set(89, { from: accounts[0] });
 
     // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+    const storedData = await aidTraceInstance.get.call();
 
     assert.equal(storedData, 89, "The value 89 was not stored.");
   });
